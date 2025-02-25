8a81fcb0121591f6466667e32d27010ace744621
costing-app <saleek4@gmail.com>
2025-02-25 16:40:59 +0530

    Updated port for Render deployment

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def download_result():
 import os
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render
+    app.run(host="0.0.0.0", port=port, debug=True)
+
